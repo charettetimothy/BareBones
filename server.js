d8283ad1e776d45bbdda7162edae906f72ac2b5a
Timothy A. Charette <charettetimothy@gmail.com>
2019-03-12 19:21:28 -0400

    added connection info in server.js

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.use(express.json())
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
 }
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'muowdopceqgxjn2b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'g5abz2zhkr8jogv0',
+    password: 'ru69102afx1l9akt',
+    database: 'oh4v074oxt5i9wen'
+  })
+}
 // Add routes
 app.use(passport.initialize());
 app.use(passport.session());
